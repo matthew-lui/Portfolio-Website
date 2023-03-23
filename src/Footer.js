4b179cca0ef28a020d5a54d3294951cbf175cacc
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2023-03-23 11:50:05 -0400

    add looms

--- src/Footer.js
+++ src/Footer.js
@@ -4,10 +4,13 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
+    
     <div className="content-container">
+   
     <footer className="bg-blue-900 text-white py-6 fixed bottom-0 inset-x-0 mt-25">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-center">
+      
           <a
             href="https://www.linkedin.com/in/lui-matthewm/"
             target="_blank"
