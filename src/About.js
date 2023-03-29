57101b5862a7ebabb26a02223346bb59479c5b54
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2023-03-29 11:46:07 -0400

    moved resume position

--- src/About.js
+++ src/About.js
@@ -8,8 +8,7 @@ function About(props) {
 <section id="about-me" className="container mx-auto px-4 py-8 flex flex-col items-center pt-24">
   <h2 className="text-2xl font-semibold mb-4 text-center"></h2>
   <p className="mb-4">
-    Hi there! I'm a Full Stack Software Engineer who loves building React
-    applications. My toolbox is filled with skills like JavaScript, HTML,
+    I'm a Full Stack Software Engineer who loves applications. My toolbox is filled with skills like JavaScript, HTML,
     CSS, React, Ruby on Rails, and PostgreSQL. I'm always excited to dive
     into software engineering projects and contribute to the growth of the
     companies I work with.
--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,13 @@ import NYCSkyline from './NYCSkyline.jpg';
 function Home(props) {
   return (
     <div className="flex flex-col items-center justify-center pt-10 md:pt-24">
-      <img className="h-80 w-full md:w-auto" src={NYCSkyline}></img>
+      <img className="h-60 w-full md:w-auto" src={NYCSkyline}></img>
       <section id="home" className="container mx-auto px-0 py-2 md:py-10 text-center md:px-20">
-        <h1 className="text-3xl font-bold mb-7 sm:text-4xl">FULL STACK SOFTWARE ENGINEER</h1>
+        <h1 className="text-3xl font-bold mb-0 sm:text-3xl">FULL STACK SOFTWARE ENGINEER</h1>
         <div>
+        <h1 className="mt-3 mb-5">
+          <ResumeModal />
+        </h1>
           <h2 className="text-2xl font-bold mb-2 sm:text-xl">
             Welcome to My Portfolio Website
           </h2>
@@ -29,14 +32,11 @@ function Home(props) {
         </p>
         <NavLink
           to="/portfolio"
-          className="text-xl font-semibold inline-block text-blue-500 hover:text-blue-700"
+          className="text-l font-semibold inline-block text-blue-500 hover:text-blue-700"
         >
           Explore My Portfolio &rarr;
         </NavLink>
 
-        <h1 className="mt-10 mb-10">
-          <ResumeModal />
-        </h1>
       </section>
     </div>
   );
